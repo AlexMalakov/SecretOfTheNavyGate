456621b399ab24a570ebf7da9f4a1df0dfbd086c
Alex Malakov <alexander.malakov@gmail.com>
2025-07-25 17:56:40 -0400

    spitter blinking

--- OuterJones/Assets/Scripts/Rooms/Gear/GearRoom.cs
+++ OuterJones/Assets/Scripts/Rooms/Gear/GearRoom.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class GearRoom : Room
 {
-    
+    [SerializeField] private List<AlternatingSpitter> spitters;
+
+    public override void onEnter() {
+        base.onEnter();
+
+        foreach(AlternatingSpitter s in this.spitters) {
+            s.onPlayerEnter();
+        }
+    }
+
+    public override void onExit() {
+        base.onExit(); 
+
+        foreach(AlternatingSpitter s in this.spitters) {
+            s.onPlayerExit();
+        }
+    }
 }
