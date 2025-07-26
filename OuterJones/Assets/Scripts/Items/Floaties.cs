ac2007f699e47ebfb0826f05e4bd72bdcf3a35d9
Alex Malakov <alexander.malakov@gmail.com>
2025-07-26 00:37:28 -0400

    floaties fix

--- OuterJones/Assets/Scripts/Items/Floaties.cs
+++ OuterJones/Assets/Scripts/Items/Floaties.cs
@@ -7,18 +7,28 @@ public class Floaties : Item
     // private Player player;
 
 
+    private Canal[] allCanals;
+
+    public void Awake() {
+        allCanals = FindObjectsOfType<Canal>(true);
+    }
     //TODO: player with floaties should be able to cross through
 
     //ignore collisions with canal Water
     public override void equip() {
         //should ignore collisions with canals (so this wont work is the funny story... but ill dewal with this later)
-        Physics.IgnoreLayerCollision(4, 7, true);
-        // foreach()
+        foreach(Canal c in this.allCanals) {
+            c.getWaterCollider().GetComponents<Collider2D>()[0].isTrigger = true;
+            c.getWaterCollider().GetComponents<Collider2D>()[1].isTrigger = true;
+        }
 
     }
 
     public override void unequip() {
-        Physics.IgnoreLayerCollision(4, 7, false);
+        foreach(Canal c in this.allCanals) {
+            c.getWaterCollider().GetComponents<Collider2D>()[0].isTrigger = false;
+            c.getWaterCollider().GetComponents<Collider2D>()[1].isTrigger = false;
+        }
     }
 
 
