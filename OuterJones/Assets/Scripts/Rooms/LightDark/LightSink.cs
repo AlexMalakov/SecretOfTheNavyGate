99d8b7a6b7bdc2a914aca181196c8b68f8171103
Alex Malakov <alexander.malakov@gmail.com>
2025-07-28 15:47:08 -0400

    light receiver written

--- OuterJones/Assets/Scripts/Rooms/LightDark/LightSink.cs
+++ OuterJones/Assets/Scripts/Rooms/LightDark/LightSink.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class LightSink : MonoBehaviour
 {
     private bool beamed = false;
-    [SerializeField] DoorDirection receiveBeamFrom;
+    [SerializeField] private DoorDirection receiveBeamFrom;
+    [SerializeField] private Effectable effectable;
+    [SerializeField] private GameObject beamedSprite;
+    [SerializeField] private GameObject notBeamedSprite;
 
     public void activate(DoorDirection beamFrom) {
-
         if(Door.rotateDoorDirection(Door.rotateDoorDirection(beamFrom, true), true) == this.receiveBeamFrom) {
             this.beamed = true;
+            this.effectable.onEffect();
+            this.beamedSprite.SetActive(true);
+            this.beamedSprite.SetActive(false);
         }
     }
 
     public void deactivate() {
         this.beamed = false;
+        this.beamedSprite.SetActive(false);
+        this.beamedSprite.SetActive(true);
     }
 
     public bool getActive() {
