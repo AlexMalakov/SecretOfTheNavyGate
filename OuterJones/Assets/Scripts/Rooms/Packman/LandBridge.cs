98dce15de2852ca039c8a4c14282ce3cffc83f50
Alex Malakov <alexander.malakov@gmail.com>
2025-07-30 15:45:23 -0400

    l4 land bridge done

--- OuterJones/Assets/Scripts/Rooms/Packman/LandBridge.cs
+++ OuterJones/Assets/Scripts/Rooms/Packman/LandBridge.cs
@@ -13,11 +13,9 @@ public class LandBridge : MonoBehaviour
     //basically if u want to cross the bridge u have to hit a collider first
     public void notifyOfPlayer(bool isUpperLevel) {
         if(playerLevel && !isUpperLevel) {
-            rend.sortingLayerName = this.playerOnBottomLayer;
             this.playerTravelingAbove.SetActive(false);
             this.playerTravelingBelow.SetActive(true);
         } else if(!playerLevel && isUpperLevel) {
-            rend.sortingLayerName = this.playerOnUpperLayer;
             this.playerTravelingAbove.SetActive(true);
             this.playerTravelingBelow.SetActive(false);
         }
