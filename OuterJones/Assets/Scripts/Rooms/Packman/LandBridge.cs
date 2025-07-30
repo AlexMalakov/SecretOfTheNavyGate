0d31125a61b28dc7cb9e4efdaecd2b91dd3f4917
Alex Malakov <alexander.malakov@gmail.com>
2025-07-29 22:42:26 -0400

    updated script

--- OuterJones/Assets/Scripts/Rooms/Packman/LandBridge.cs
+++ OuterJones/Assets/Scripts/Rooms/Packman/LandBridge.cs
@@ -4,26 +4,22 @@ using UnityEngine;
 
 public class LandBridge : MonoBehaviour
 {
-    [SerializeField] private string playerOnUpperLayer = "Enviroment";
-    [SerializeField] private string playerOnBottomLayer = "FrontForeground";
-
-    [SerializeField] private GameObject railingCollider;
-    [SerializeField] private GameObject groundCollider;
+    [SerializeField] private GameObject playerTravelingAbove;
+    [SerializeField] private GameObject playerTravelingBelow;
 
 
     private bool playerLevel = true;
 
     //basically if u want to cross the bridge u have to hit a collider first
     public void notifyOfPlayer(bool isUpperLevel) {
-        Renderer rend = GetComponent<Renderer>();
         if(playerLevel && !isUpperLevel) {
             rend.sortingLayerName = this.playerOnBottomLayer;
-            this.railingCollider.SetActive(false); //there shouldn't be a collider keeping the player on the bridge
-            this.groundCollider.SetActive(true);
+            this.playerTravelingAbove.SetActive(false);
+            this.playerTravelingBelow.SetActive(true);
         } else if(!playerLevel && isUpperLevel) {
             rend.sortingLayerName = this.playerOnUpperLayer;
-            this.railingCollider.SetActive(true); //there should be a collider keeping the player on the bridge
-            this.groundCollider.SetActive(false);
+            this.playerTravelingAbove.SetActive(true);
+            this.playerTravelingBelow.SetActive(false);
         }
     }
 }
