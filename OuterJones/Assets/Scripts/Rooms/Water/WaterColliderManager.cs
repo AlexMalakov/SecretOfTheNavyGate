a05648e616a40eacf50e5a177a16f96ca783a46b
Alex Malakov <alexander.malakov@gmail.com>
2025-08-03 18:19:34 -0400

    water collider attempt to solve the bridge grate floaties issue

---
+++ OuterJones/Assets/Scripts/Rooms/Water/WaterColliderManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterColliderManager : MonoBehaviour
+{
+    private bool floatiesAquired;
+
+    private bool onGrate;
+
+    private bool onBridge;
+
+    public void onFloatiesAquired() {
+        this.floatiesAquired = true;
+
+        this.setColliderTriggerStatus();
+    }
+
+    public void setBridgeStatus(bool status) {
+        this.onBridge = status;
+
+        this.setColliderTriggerStatus();
+    }
+
+    public void setGrateStatus(bool status) {
+        this.onGrate = status;
+
+        this.setColliderTriggerStatus();
+    }
+
+    private void setColliderTriggerStatus() {
+        this.GetComponent<CompositeCollider2D>().isTrigger = floatiesAquired || onGrate || onBridge;
+    }
+}
