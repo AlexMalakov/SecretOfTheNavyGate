0102586c2309042b80bdc4f58e687fd42eff14da
Alex Malakov <alexander.malakov@gmail.com>
2025-08-08 18:48:06 -0400

    stopping and starting is back

--- OuterJones/Assets/Scripts/Rooms/Packman/Mummy.cs
+++ OuterJones/Assets/Scripts/Rooms/Packman/Mummy.cs
@@ -39,9 +39,9 @@ public class Mummy : MonoBehaviour
         if(isAwake) {
             agent.SetDestination(this.player.transform.position);
 
-            // if (agent.isStopped != !this.player.gameObject.GetComponent<PlayerController>().isPlayerMoving()) {
-            //     agent.isStopped = !this.player.gameObject.GetComponent<PlayerController>().isPlayerMoving();
-            // }
+            if (agent.isStopped != !this.player.gameObject.GetComponent<PlayerController>().isPlayerMoving()) {
+                agent.isStopped = !this.player.gameObject.GetComponent<PlayerController>().isPlayerMoving();
+            }
         }
         
     }
