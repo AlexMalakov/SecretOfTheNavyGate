d9da5c7b5edfb512ea6238c213a4e704604a4cb7
Alex Malakov <alexander.malakov@gmail.com>
2025-08-12 15:24:35 -0400

    updated regular buttons

--- OuterJones/Assets/Scripts/Rooms/Packman/TemporaryEffectableButton.cs
+++ OuterJones/Assets/Scripts/Rooms/Packman/TemporaryEffectableButton.cs
@@ -5,20 +5,33 @@ using UnityEngine;
 
 //button is currently a terrible script thats desgined to exist in only packman rooms
 //so this should be more abstract
+//ok i kinda dont want to rewrite it tho so maybe not so temporary after all >:)
 public class TemporaryEffectableButton : MonoBehaviour, Effectable
 {
     [SerializeField] private GameObject effObj;
 
     [SerializeField] private bool requiresActivation = false; 
 
+    [SerializeField] private GameObject defaultSprite;
+    [SerializeField] private GameObject pressedSprite;
+
 
     void OnTriggerEnter2D(Collider2D other) {
         if(!this.requiresActivation && other.gameObject.GetComponent<Player>() != null) {
             this.effObj.GetComponent<Effectable>().onEffect();
+            this.defaultSprite.SetActive(false);
+            this.pressedSprite.SetActive(true);
+
+            Invoke(nameof(resetSprite), .5f);
         }
     }
 
     public void onEffect() {
         this.requiresActivation = false;
+    }
+
+    private void resetSprite() {
+        this.defaultSprite.SetActive(true);
+        this.pressedSprite.SetActive(false);
     }
 }
