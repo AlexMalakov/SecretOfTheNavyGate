305c40398e1e8038a5337b3eda51be60c70edc9f
Alex Malakov <alexander.malakov@gmail.com>
2025-08-12 16:36:49 -0400

    fixed packman puzzle bug

--- OuterJones/Assets/Scripts/Rooms/Packman/PackmanCornerPuzzleManager.cs
+++ OuterJones/Assets/Scripts/Rooms/Packman/PackmanCornerPuzzleManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PackmanCornerPuzzleManager : MonoBehaviour, RoomUpdateListener
 {
+
     [SerializeField] private PackmanCornerPiece topLeftPiece;
     [SerializeField] private PackmanCornerPiece topRightPiece;
     [SerializeField] private PackmanCornerPiece bottomLeftPiece;
@@ -18,25 +19,23 @@ public class PackmanCornerPuzzleManager : MonoBehaviour, RoomUpdateListener
         this.layout.addRoomUpdateListener(this);
     }
 
-
-
     public void onRoomUpdate(List<Room> rooms) {
         if(completed) {
             return;
         }
-        bool TlrNeighbors = this.areNeighbors(this.topLeftPiece, this.topRightPiece);
+        bool TlrNeighbors = this.areNeighbors(this.topLeftPiece, this.topRightPiece) && this.correctOrientation(this.topLeftPiece, this.topRightPiece, false);
         this.topLeftPiece.setActivationStatus(false, TlrNeighbors);
         this.topRightPiece.setActivationStatus(false, TlrNeighbors);
 
-        bool LtbNeighbors = this.areNeighbors(this.topLeftPiece, this.bottomLeftPiece);
+        bool LtbNeighbors = this.areNeighbors(this.topLeftPiece, this.bottomLeftPiece) && this.correctOrientation(this.topLeftPiece, this.bottomLeftPiece, true);
         this.topLeftPiece.setActivationStatus(true, LtbNeighbors);
         this.bottomLeftPiece.setActivationStatus(true, LtbNeighbors);
 
-        bool RtbNeighbors = this.areNeighbors(this.topRightPiece, this.bottomRightPiece);
+        bool RtbNeighbors = this.areNeighbors(this.topRightPiece, this.bottomRightPiece) && this.correctOrientation(this.topRightPiece, this.bottomRightPiece, true);
         this.topRightPiece.setActivationStatus(true, RtbNeighbors);
         this.bottomRightPiece.setActivationStatus(true, RtbNeighbors);
 
-        bool BlrNeighbors = this.areNeighbors(this.bottomLeftPiece, this.bottomRightPiece);
+        bool BlrNeighbors = this.areNeighbors(this.bottomLeftPiece, this.bottomRightPiece) && this.correctOrientation(this.bottomLeftPiece, this.bottomRightPiece, false);
         this.bottomLeftPiece.setActivationStatus(false, BlrNeighbors);
         this.bottomRightPiece.setActivationStatus(false, BlrNeighbors);
 
@@ -55,5 +54,9 @@ public class PackmanCornerPuzzleManager : MonoBehaviour, RoomUpdateListener
             || (p1.getRoomCoords().x - p2.getRoomCoords().x == 0 && Mathf.Abs(p1.getRoomCoords().y - p2.getRoomCoords().y) == 1)
             || (((p1.getRoomCoords().x == 0 && p2.getRoomCoords().x == RoomsLayout.ROOM_GRID_X-1) || (p1.getRoomCoords().x == RoomsLayout.ROOM_GRID_X-1 && p2.getRoomCoords().x == 0)) && p1.getRoomCoords().y - p2.getRoomCoords().y == 0)
             || (p1.getRoomCoords().x - p2.getRoomCoords().x == 0 && ((p1.getRoomCoords().y == 0 && p2.getRoomCoords().y == RoomsLayout.ROOM_GRID_X-1) || (p1.getRoomCoords().y == RoomsLayout.ROOM_GRID_X-1 && p2.getRoomCoords().y == 0)));
+    }
+
+    private bool correctOrientation(PackmanCornerPiece p1, PackmanCornerPiece p2, bool vertical) {
+        return vertical ? (p1.getRoomCoords().y - p2.getRoomCoords().y == 1) : (p1.getRoomCoords().x - p2.getRoomCoords().x == -1);
     }
 }
