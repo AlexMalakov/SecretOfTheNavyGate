fb707a9bb2ff412064067cb84c94fb75e664ef11
Alex Malakov <alexander.malakov@gmail.com>
2025-09-15 15:41:45 -0400

    rewrote corner piece code in a way that lets rotations be possible

--- OuterJones/Assets/Scripts/Rooms/Packman/PackmanCornerPiece.cs
+++ OuterJones/Assets/Scripts/Rooms/Packman/PackmanCornerPiece.cs
@@ -5,31 +5,42 @@ using UnityEngine;
 public class PackmanCornerPiece : MonoBehaviour
 {
     [SerializeField] private PackmanRoom roomOfOrigin;
+    [SerializeField] private CornerPosition currentPos;
 
-    [SerializeField] private GameObject vertLit;
-    [SerializeField] private GameObject horizLit;
+    [SerializeField] private GameObject cwLit;
+    [SerializeField] private GameObject ccwLit;
 
-    private bool vertActivated = false;
-    private bool horizActivated = false;
+    private bool cwActivated = false;
+    private bool ccwActivated = false;
 
     public RoomCoords getRoomCoords() {
         return this.roomOfOrigin.getPosition();
     }
 
-    public void setActivationStatus(bool verticalNeighbor, bool status) {
-        if(verticalNeighbor) {
-            vertActivated = status;
-            vertLit.SetActive(status);
-            //light up
-        } else {
-            horizActivated = status;
-            horizLit.SetActive(status);
-            //light up
-        }
+    public void deactivate() {
+        this.cwActivated = false;
+        this.ccwActivated = false;
+        this.cwLit.SetActive(false);
+        this.ccwLit.SetActive(false);
+    }
+
+    public void activate(bool cwNeighbor) {
+        this.cwActivated = cwActivated || cwNeighbor;
+        this.ccwActivated = ccwActivated || !cwNeighbor;
+        this.cwLit.SetActive(this.cwActivated);
+        this.ccwLit.SetActive(this.ccwActivated);
+    }
+
+    public CornerPosition getCornerPosition() {
+        return this.currentPos;
     }
 
 
     public bool isFullyActivated() {
-        return vertActivated && horizActivated;
+        return cwActivated && ccwActivated;
+    }
+
+    public void rotate90(bool clockwise) {
+        this.currentPos = PackmanCornerPuzzleManager.rotateCornerPosition(clockwise, this.currentPos);
     }
 }
--- OuterJones/Assets/Scripts/Rooms/Packman/PackmanCornerPuzzleManager.cs
+++ OuterJones/Assets/Scripts/Rooms/Packman/PackmanCornerPuzzleManager.cs
@@ -2,14 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum CornerPosition {
+    TL = 0, TR = 1, BR = 2, BL = 3
+}
+
 public class PackmanCornerPuzzleManager : MonoBehaviour, RoomUpdateListener
 {
-
-    [SerializeField] private PackmanCornerPiece topLeftPiece;
-    [SerializeField] private PackmanCornerPiece topRightPiece;
-    [SerializeField] private PackmanCornerPiece bottomLeftPiece;
-    [SerializeField] private PackmanCornerPiece bottomRightPiece;
-
+    [SerializeField] private List<PackmanCornerPiece> cornerPieces;
     [SerializeField] private RoomsLayout layout;
 
     [SerializeField] private GameObject effectableObj;
@@ -20,43 +19,76 @@ public class PackmanCornerPuzzleManager : MonoBehaviour, RoomUpdateListener
     }
 
     public void onRoomUpdate(List<Room> rooms) {
+        //check that all corners are in the correct spot
         if(completed) {
             return;
         }
-        bool TlrNeighbors = this.areNeighbors(this.topLeftPiece, this.topRightPiece) && this.correctOrientation(this.topLeftPiece, this.topRightPiece, false);
-        this.topLeftPiece.setActivationStatus(false, TlrNeighbors);
-        this.topRightPiece.setActivationStatus(false, TlrNeighbors);
 
-        bool LtbNeighbors = this.areNeighbors(this.topLeftPiece, this.bottomLeftPiece) && this.correctOrientation(this.topLeftPiece, this.bottomLeftPiece, true);
-        this.topLeftPiece.setActivationStatus(true, LtbNeighbors);
-        this.bottomLeftPiece.setActivationStatus(true, LtbNeighbors);
+        Dictionary<CornerPosition, List<PackmanCornerPiece>> keyPos = new Dictionary<CornerPosition, List<PackmanCornerPiece>>();
+        keyPos.Add(CornerPosition.TL, new List<PackmanCornerPiece>());
+        keyPos.Add(CornerPosition.TR, new List<PackmanCornerPiece>());
+        keyPos.Add(CornerPosition.BL, new List<PackmanCornerPiece>());
+        keyPos.Add(CornerPosition.BR, new List<PackmanCornerPiece>());
 
-        bool RtbNeighbors = this.areNeighbors(this.topRightPiece, this.bottomRightPiece) && this.correctOrientation(this.topRightPiece, this.bottomRightPiece, true);
-        this.topRightPiece.setActivationStatus(true, RtbNeighbors);
-        this.bottomRightPiece.setActivationStatus(true, RtbNeighbors);
+        foreach(PackmanCornerPiece piece in cornerPieces) {
+            keyPos[piece.getCornerPosition()].Add(piece);
+        }
 
-        bool BlrNeighbors = this.areNeighbors(this.bottomLeftPiece, this.bottomRightPiece) && this.correctOrientation(this.bottomLeftPiece, this.bottomRightPiece, false);
-        this.bottomLeftPiece.setActivationStatus(false, BlrNeighbors);
-        this.bottomRightPiece.setActivationStatus(false, BlrNeighbors);
+        this.checkNeighbors(keyPos[CornerPosition.TL], keyPos[CornerPosition.TR], keyPos[CornerPosition.BL]);
+        this.checkNeighbors(keyPos[CornerPosition.TR], keyPos[CornerPosition.BR], keyPos[CornerPosition.TL]);
+        this.checkNeighbors(keyPos[CornerPosition.BL], keyPos[CornerPosition.TL], keyPos[CornerPosition.BR]);
+        this.checkNeighbors(keyPos[CornerPosition.BR], keyPos[CornerPosition.BL], keyPos[CornerPosition.TR]);
 
-        if(this.topLeftPiece.isFullyActivated() 
-                && this.topRightPiece.isFullyActivated()
-                && this.bottomLeftPiece.isFullyActivated() 
-                && this.bottomRightPiece.isFullyActivated()) {
 
-            completed = true;
-            effectableObj.gameObject.GetComponent<Effectable>().onEffect();
+        foreach(PackmanCornerPiece piece in cornerPieces) {
+            if(!piece.isFullyActivated()) {
+                return;
+            }
+        }
+        completed = true;
+        effectableObj.gameObject.GetComponent<Effectable>().onEffect();
+    }
+
+    private void checkNeighbors(List<PackmanCornerPiece> pieces, List<PackmanCornerPiece> cwNeighbors, List<PackmanCornerPiece> ccwNeighbors) {
+        foreach(PackmanCornerPiece piece in pieces) {
+            piece.deactivate();
+
+            foreach(PackmanCornerPiece cwNeighbor in cwNeighbors) {
+                if(areNeighbors(piece, cwNeighbor, isHorizontalOffsetDirection(piece.getCornerPosition(), true))) {
+                    piece.activate(true);
+                    break;
+                }
+            }
+
+            foreach(PackmanCornerPiece ccwNeighbor in ccwNeighbors) {
+                if(areNeighbors(piece, ccwNeighbor, isHorizontalOffsetDirection(piece.getCornerPosition(), false))) {
+                    piece.activate(false);
+                    break;
+                }
+            }
         }
     }
 
-    private bool areNeighbors(PackmanCornerPiece p1, PackmanCornerPiece p2) {
-        return (Mathf.Abs(p1.getRoomCoords().x - p2.getRoomCoords().x) == 1 && p1.getRoomCoords().y - p2.getRoomCoords().y == 0)
-            || (p1.getRoomCoords().x - p2.getRoomCoords().x == 0 && Mathf.Abs(p1.getRoomCoords().y - p2.getRoomCoords().y) == 1)
-            || (((p1.getRoomCoords().x == 0 && p2.getRoomCoords().x == RoomsLayout.ROOM_GRID_X-1) || (p1.getRoomCoords().x == RoomsLayout.ROOM_GRID_X-1 && p2.getRoomCoords().x == 0)) && p1.getRoomCoords().y - p2.getRoomCoords().y == 0)
-            || (p1.getRoomCoords().x - p2.getRoomCoords().x == 0 && ((p1.getRoomCoords().y == 0 && p2.getRoomCoords().y == RoomsLayout.ROOM_GRID_X-1) || (p1.getRoomCoords().y == RoomsLayout.ROOM_GRID_X-1 && p2.getRoomCoords().y == 0)));
+    private bool isHorizontalOffsetDirection(CornerPosition position, bool clockwise) {
+        int intPos = (int)position;
+        return (intPos % 2 == 0 && clockwise) || (intPos % 2 == 1 && !clockwise);
+    }
+
+    private bool areNeighbors(PackmanCornerPiece p1, PackmanCornerPiece p2, bool isHorizontal) {
+        RoomCoords p1RC = p1.getRoomCoords();
+        RoomCoords p2RC = p2.getRoomCoords();
+
+        if(isHorizontal) {
+            return p1RC.y - p2RC.y == 0 && (Mathf.Abs(p1RC.x - p2RC.x) == 0 || (p1RC.x == 0 && p2RC.x == RoomsLayout.ROOM_GRID_X-1) || (p2RC.x == 0 && p1RC.x == RoomsLayout.ROOM_GRID_X-1));
+        }
+        return p1RC.x - p2RC.x == 0 && (Mathf.Abs(p1RC.y - p2RC.y) == 0 || (p1RC.y == 0 && p2RC.y == RoomsLayout.ROOM_GRID_X-1) || (p2RC.y == 0 && p1RC.y == RoomsLayout.ROOM_GRID_X-1));
     }
 
     private bool correctOrientation(PackmanCornerPiece p1, PackmanCornerPiece p2, bool vertical) {
         return vertical ? (p1.getRoomCoords().y - p2.getRoomCoords().y == 1) : (p1.getRoomCoords().x - p2.getRoomCoords().x == -1);
+    }
+
+    public static CornerPosition rotateCornerPosition(bool clockwise, CornerPosition pos) {
+        return (CornerPosition)(((int)pos + 4 + (clockwise ? 1 : -1))%4);
     }
 }
